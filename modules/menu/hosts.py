a6760e2d9a9c29b4d329b92f5ee06f973cc816c1
16Shadows <egor.0.00@mail.ru>
2024-07-20 18:14:08 +0500

    feat: Добавлен модуль menu.hosts
    Модуль menu.hosts реализует контексты отображения меню:
    Класс SimpleConsoleMenuHost реализует отображение меню в консоли с выводом нумерованных пунктов и выбором пункта вводом его номера.

---
+++ modules/menu/hosts.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import Self
+from .core import MenuBase, MenuEntryBase, MenuHostBase
+
+class SimpleConsoleMenuHost(MenuHostBase):
+    """Реализация MenuHost, выводящая пункты меню построчно в консоль и предлагающая пользователю ввести номер пункта."""
+    def run(self: Self, enterAt: MenuBase | None = None) -> None:
+        if enterAt is not None:
+            self.menuStack.clear()
+            self.push(enterAt)
+        
+        while len(self.menuStack) > 0:
+            currentMenu : MenuBase = self.current()
+            currentMenuEntries : list[MenuEntryBase] = currentMenu.entries
+
+            while True:
+                print(currentMenu.text)
+                for i in range(0, len(currentMenuEntries)):
+                    print(i+1, ". ", currentMenuEntries[i].text)
+                try:
+                    option = int(input("Введите номер пункта: "))
+                    if option < 1 or option > len(currentMenuEntries):
+                        raise ValueError
+                except ValueError:
+                    print(option, " - некорректный номер пункта.") # type: ignore
+                    continue
+                currentMenuEntries[option - 1].on_selected(self)
+                break
