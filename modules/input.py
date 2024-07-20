516eb7b1fb75c0bb11b57369d47cfe95d51137c9
16Shadows <egor.0.00@mail.ru>
2024-07-20 18:24:10 +0500

    feat: Добавлен модуль input.
    Модуль input реализует методы для обработки пользовательского ввода.
    Метод input_validated используется для запрашивания ввода у пользователя до ввода значения, которое успешно преобразуется с помощью указанной функции преобразования и успешно проходит валидацию указанной функцией-валидатором.
    Метод converter_string является методом-преобразователем, который стирает из начала и конца пользовательской строки whitespace-символы.
    Метод converter_int является методом-преобразователем, который преобразует пользовательскую строку
    Метод validator_string_not_empty является методом-валидатором, проверяющим, что строка не пустая.
    Метод validator_always является методом-валидатором, который принимает любое значение как валидное.

---
+++ modules/input.py
@@ -0,0 +1,37 @@
+from typing import Callable
+
+def input_validated[T](prompt: str, convert: Callable[[str], T], validate: Callable[[T], bool], errorMessage: str) -> T:
+    '''Запрашивает у пользователя ввод, пока пользователь не введёт значение,
+    успешно преобразуемое функцией convert и валидируемое функцией validate
+    
+    Аргументы:
+    prompt : str -- Отображаемое при запросе ввода сообщение
+    convert : Callable[[str], T] -- функция-конвертер для преобразования входной строки к желаемому типу. Может поднимать исключение ValueError.
+    validate : Callable[[T], bool] -- функция-валидатор для валидации преобразованного значения. Должна возвращать True, если значение валидно, False иначе.
+    errorMessage : str -- сообщение, которое будет выведено, если будет поднято исключение ValueError или validate вернёт False.
+    '''
+    while True:
+        try:
+            user_input = input(prompt)
+            result : T = convert(user_input)
+            if not validate(result):
+                raise ValueError
+            return result
+        except ValueError:
+            print(errorMessage)
+
+def converter_string(x : str) -> str:
+    '''Функция-конвертер для input_validated, убирающая whitespace-символы из начала и конца строки'''
+    return x.strip()
+
+def converter_int(x: str) -> int:
+    '''Функция-конвертер для input_validated, парсящая строку как целое число'''
+    return int(x)
+
+def validator_string_not_empty(x : str) -> bool:
+    '''Функция-валидатор для input_validated, проверяющая, не пустая ли строка'''
+    return len(x) > 0
+
+def validator_always(_ : object) -> bool:
+    '''Функция-валидатор для input_validate, принимающая любое значение как валидное'''
+    return True
